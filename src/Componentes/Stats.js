f9440903ad29c143c0319c3bac9c0a9aa71cf711
nzr_pe <joseluis.von@gmail.com>
2022-09-25 02:18:27 -0500

    stats-maquetacion

--- src/Componentes/Stats.js
+++ src/Componentes/Stats.js
@@ -1,9 +1,73 @@
 import React from 'react';
-import '../css/navbar.css';
+import '../css/stats.css';
 
 export function Stats(){
 
     return (
-        <p> ESTADISTICA AQUI</p>
+        <div className='container'>
+                <div className='row justify-content-center'>
+                        <div className='col-8 text-center'>
+                                <h2 className='title'> ESTADISTICAS POR TEMPORADA</h2>
+                        </div>
+
+                </div>
+                <div className='row justify-content-center' id='matchcontent'>
+                <div className='col-6 text-center' id='contentstats'>
+                        <h3 className='subtitle' >ESTADISTICAS GLOBALES</h3>
+                        <div className='row justify-content-center' >
+                       <div className='col-4'>
+                        <div className='row justify-content-center'>
+                                   <h4 className='litletitle'>Goles</h4>     
+                        </div>
+                                <p>500</p>      
+                       </div>         
+                 </div>
+                 <div className='row justify-content-center' >
+                       <div className='col-4'>
+                        <div className='row justify-content-center'>
+                                   <h4 className='litletitle'>Tiros</h4>     
+                        </div>
+                                <p>500</p>      
+                       </div>         
+                 </div> 
+                 <div className='row justify-content-center' >
+                       <div className='col-4'>
+                        <div className='row justify-content-center'>
+                                   <h4 className='litletitle'>Tiros al Arco</h4>     
+                        </div>
+                                <p>500</p>      
+                       </div>         
+                 </div>      
+                 <div className='row justify-content-center' >
+                       <div className='col-4'>
+                        <div className='row justify-content-center'>
+                                   <h4 className='litletitle'>Pases</h4>     
+                        </div>
+                                <p>500</p>      
+                       </div>         
+                 </div>        
+                 <div className='row justify-content-center' >
+                       <div className='col-4'>
+                        <div className='row justify-content-center'>
+                                   <h4 className='litletitle'>Faltas</h4>     
+                        </div>
+                                <p>500</p>      
+                       </div>         
+                 </div>               
+                </div>
+                 <div className='row justify-content-center'>
+                 <div className='col-6 text-center' id='contentstats'>
+                 <h4 className='subtitle'>Promedio de Conversion de Goles</h4>   
+                 <div className='row justify-content-center'>
+                        <div className='col-12'>      
+                                <p>que fuyente </p>
+                        </div>                       
+                 </div>
+                 </div>       
+                 </div>
+                </div>
+
+                
+        </div>
     );
 }
